ffff4b193f26cac826c2d44be54b7779bc067a42
jrwaka <j.rwaka@alustudent.com>
2024-08-28 13:58:29 +0200

    New updates

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,17 +2,17 @@ import React from "react";
 export default function Index() {
     return(
         <div>
-            <div className="py-3 flex justify-center items-center text-center w-full bg-yellow-400 text-sm">
-                <p className="flex items-center"><svg xmlns="http://www.w3.org/2000/svg" height="28px" viewBox="0 -960 960 960" width="20px" fill="black"><path d="M192-216v-72h48v-240q0-87 53.5-153T432-763v-53q0-20 14-34t34-14q20 0 34 14t14 34v53q85 16 138.5 82T720-528v240h48v72H192Zm288-276Zm-.21 396Q450-96 429-117.15T408-168h144q0 30-21.21 51t-51 21ZM312-288h336v-240q0-70-49-119t-119-49q-70 0-119 49t-49 119v240Z"/></svg><b className="font-semibold">Igishya!</b> Umwaka wa mituweli 2024/2025 watangiye. Ishyurira umuryango wawe <b><u>hano</u></b></p>
+            <div className="py-3 flex justify-center items-center text-center w-full bg-yellow-400 text-sm fixed top-0 left-0 right-0">
+                <p className="flex items-center"><svg xmlns="http://www.w3.org/2000/svg" height="28px" viewBox="0 -960 960 960" width="20px" fill="black"><path d="M192-216v-72h48v-240q0-87 53.5-153T432-763v-53q0-20 14-34t34-14q20 0 34 14t14 34v53q85 16 138.5 82T720-528v240h48v72H192Zm288-276Zm-.21 396Q450-96 429-117.15T408-168h144q0 30-21.21 51t-51 21ZM312-288h336v-240q0-70-49-119t-119-49q-70 0-119 49t-49 119v240Z"/></svg><h1 className="font-semibold">Igishya!</h1> Umwaka wa mituweli 2024/2025 watangiye. Ishyurira umuryango wawe  <a href="#" className="font-semibold underline"> hano</a></p>
             </div>
 
-            <div className="flex flex-row py-4 bg-blue-600 shadow-xl justify-around">
+            <div className="flex flex-row py-4 bg-blue-600 shadow-2xl justify-around fixed top-[50px] left-0 right-0">
                 <div className="w-32 flex flex-row items-center">
                 <img src="irembo-gov.svg" alt=""/>
                 </div>
 
             <div className="flex flex-row gap-5 items-center text-white text-sm font-semibold">
-                    <button className="flex flex-row py-2 px-4 gap-1 items-center bg-blue-400 rounded-sm"><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e8eaed"><path d="M480-144v-72h264v-528H480v-72h264q29.7 0 50.85 21.15Q816-773.7 816-744v528q0 29.7-21.15 50.85Q773.7-144 744-144H480Zm-72-168-51-51 81-81H144v-72h294l-81-81 51-51 168 168-168 168Z"/></svg><span>Kwinjira</span></button>
+                    <button className="flex flex-row py-2 px-4 gap-1 items-center hover:bg-blue-400 rounded-sm transition-all duration-100 ease-in-out"><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e8eaed"><path d="M480-144v-72h264v-528H480v-72h264q29.7 0 50.85 21.15Q816-773.7 816-744v528q0 29.7-21.15 50.85Q773.7-144 744-144H480Zm-72-168-51-51 81-81H144v-72h294l-81-81 51-51 168 168-168 168Z"/></svg><span>Kwinjira</span></button>
 
                     <button className="flex flex-row py-2 px-4 gap-1 items-center bg-blue-400 rounded-sm"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="size-4"><path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" /></svg><span>Shaka dosiye</span></button>
                 <div className="flex flex-row items-center gap-2">
--- src/components/loginform.jsx
+++ src/components/loginform.jsx
@@ -1,13 +1,44 @@
 import React from "react";
 export default function Loginform() {
     return(
-        <div>
+        <div className="mt-48">
             <div className="flex flex-row m-20 justify-center items-center">
-                <div className="flex flex-row bg-gradient-to-t from-cyan-400 to-blue-500 justify-center items-center px-[20rem] py-[20rem] rounded-l-md">
+                <div className="bg-gradient-to-t from-blue-500 to-blue-600 w-[38rem] h-screen rounded-l-md">
+                    <div className="p-20 flex flex-col gap-6">
+                    <h1 className="font-semibold text-white text-5xl">Gufungura konti</h1>
+                    <h1 className="font-normal text-white text-wrap text-lg">Kurikiza aya mabwiriza yo kwiyandikisha kugirango udufashe kugufungurira konti</h1>
+                    <div className="flex flex-row gap-10">
+                    <span className="rounded-3xl w-6 h-6 bg-white text-center text-sm text-blue-500"><p>1</p></span>
+                    <h1 className="text-white text-sm/[16px] font-semibold">Ohereza amakuru asabwa</h1>
+                    </div>
 
+                    <div className="flex flex-row gap-10">
+                    <span className="rounded-3xl w-6 h-6 bg-blue-400 text-center text-sm text-white"><p>2</p></span>
+                    <h1 className="text-white text-sm/[16px] font-semibold">Isuzuma rya konti</h1>
+                    </div>
+
+                    <div className="flex flex-row gap-10">
+                    <span className="rounded-3xl w-6 h-6 bg-blue-400 text-center text-sm text-white"><p>3</p></span>
+                    <h1 className="text-white text-sm/[16px] font-semibold">Shyiramwo ijambo ry'ibanga ryawe</h1>
+                    </div>
+                    </div>
+                    <div className="flex flex-row px-36 py-[8rem]">
+                        <img src="reset-pwd-footer.svg" alt="" />
+                    </div>
                 </div>
 
-                <div className="flex flex-row bg-slate-300 justify-center items-center px-[10rem] py-[20rem] rounded-r-md shadow-[4px,5px,4px] shadow-2xl">
+                <div className="bg-white justify-center items-center w-[25rem] h-screen rounded-r-md shadow-2xl">
+                    <div className="py-24 px-10 flex flex-col gap-4">
+                        <h1 className="font-semibold text-lg/[18px]">Gufungura konti</h1>
+                        <h1 className="text-sm/[16px] flex">Nomero y'indangamuntu <h1 className="text-red-600">*</h1></h1>
+                        <input type="text" className="border-2 border-gray-200 rounded-md h-10 px-2 text-sm/[16px] hover:shadow-lg transition-all duration-150 ease-in-out" placeholder="Shyiramo nomero y'indangamuntu"/>
+                        <h1 className="text-sm/[16px] flex">Nomero ya telefoni (Rwanda) <h1 className="text-red-600">*</h1></h1>
+                        <input type="password" className="border-2 border-gray-200 rounded-md h-10 px-2 text-sm/[16px] hover:shadow-lg transition-all duration-150 ease-in-out" placeholder="Andika Nomero ya Telefoni"/>
+                        <button className="rounded-md h-10 px-2 bg-blue-400 text-white text-sm/[16px]">Iyandikishe</button>
+                        <h1 className="text-sm/[24px]">Mu kwiyandikisha, uremeza gukoresha <a href="#" className="text-blue-600"> amabwiriza</a> mashya ya IremboGov harimo n'ayo <a href="#" className="text-blue-600">kurinda amakuru </a>bwite.</h1>
+
+                        <h1 className="text-sm/[24px]">Usanzwe ufite konti? <a href="#" className="text-blue-600 hover:underline">Injira</a></h1>
+                    </div>
                 </div>
             </div>
         </div>
